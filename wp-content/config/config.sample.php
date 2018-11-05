eb4f04764982dd7aac13b2f8745e03676fa3b315
Mike Schinkel <mike@newclarity.net>
2018-11-05 16:06:24 -0500

    Added db[*] to project config so people won't be confused by it being missing.

--- wp-content/config/config.sample.php
+++ wp-content/config/config.sample.php
@@ -6,5 +6,14 @@ return array(
 	'error[reporting]'       => E_ALL,
 	'error[display]'         => '1',
 	'error[display_startup]' => '1',
+	'db[name]'               => 'example_db',
+	'db[user]'               => 'example_user',
+	'db[pass]'               => '1234567890abcdef',
+	'db[charset]'            => 'utf8mb4_unicode_ci',
+	'db[collate]'            => 'utf8mb4',
+
+//   These are not needed as they are Better WP-Config defaults
+//	'db[host]'               => 'localhost',
+//	'db[table_prefix]'       => 'wp_',
 );
 
