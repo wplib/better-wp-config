af593aaae6b7a94bf5bb1b66d81edab6f1af2242
Mike Schinkel <mike@newclarity.net>
2018-11-06 02:11:17 -0500

    Added a platform config file (though we might evolve this.)

---
+++ wp-content/config/config.platform.sample.php
@@ -0,0 +1,11 @@
+		<?php
+		/**
+		 * config.platform.php
+		 */
+
+		return array(
+			'environment[platform]'  => 'wordpress',
+		//	'environment[platform]'  => 'classicpress',
+		//	'environment[platform]'  => 'calmpress',
+		);
+
